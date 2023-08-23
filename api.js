9d0e493fd300fb13938a28a3ea04e1539cf9c803
redastambouli <reda.boudghenestambouli@gmail.com>
2023-08-23 12:18:54 +0100

    update 4 commit

--- api.js
+++ api.js
@@ -1,21 +1,42 @@
-
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDocs, getDoc, query, where } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4seyYITCpXMC1LJKioB8yFj-NbHrE0bY",
-  authDomain: "vanlife-9195c.firebaseapp.com",
-  projectId: "vanlife-9195c",
-  storageBucket: "vanlife-9195c.appspot.com",
-  messagingSenderId: "842928643599",
-  appId: "1:842928643599:web:7e0f35c6f491ec94df0d7a"
+    apiKey: "AIzaSyD4seyYITCpXMC1LJKioB8yFj-NbHrE0bY",
+    authDomain: "vanlife-9195c.firebaseapp.com",
+    projectId: "vanlife-9195c",
+    storageBucket: "vanlife-9195c.appspot.com",
+    messagingSenderId: "842928643599",
+    appId: "1:842928643599:web:7e0f35c6f491ec94df0d7a"
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, 'vans')
 
 
 
+export async function getVans() {
+    const querySnapshot = await getDocs(vansCollectionRef)
+    const dataArr = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    console.log(dataArr)
+    return dataArr
+}
 
-export async function getVans(id) {
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const vanSnapshot = await getDoc(docRef)
+    return {
+        ...vanSnapshot.data(),
+        id: vanSnapshot.id
+    }
+}
+
+/*export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
     if (!res.ok) {
@@ -28,8 +49,18 @@ export async function getVans(id) {
     const data = await res.json()
     return data.vans
 }
+*/
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const querySnapshot = await getDocs(q)
+    const dataArr = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return dataArr
+}
 
-export async function getHostVans(id) {
+/*export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
     const res = await fetch(url)
     if (!res.ok) {
@@ -42,6 +73,7 @@ export async function getHostVans(id) {
     const data = await res.json()
     return data.vans
 }
+*/
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
