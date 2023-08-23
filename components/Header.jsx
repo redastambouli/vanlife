eeb406906bbd9e67ec4170887b40bd1339eb524b
redastambouli <reda.boudghenestambouli@gmail.com>
2023-08-23 23:08:58 +0100

    update 5 commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import avatarIcon from '../assets/images/avatar-icon.png'
 
 export default function Header() {
     const activeStyles = {
@@ -7,36 +8,36 @@ export default function Header() {
         textDecoration: "underline",
         color: "#161616"
     }
-    
+
     function fakeLogOut() {
         localStorage.removeItem("loggedin")
     }
-    
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink 
+                <NavLink
                     to="host"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="about"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="vans"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
-                        src="../assets/images/avatar-icon.png" 
+                    <img
+                        src={avatarIcon}
                         className="login-icon"
                     />
                 </Link>
